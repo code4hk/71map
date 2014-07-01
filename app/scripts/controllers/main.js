a5e2155400c41d354f9c4ee6fd0a21ed88de6fa7
Vincent Lau <lauchunyin@gmail.com>
2014-07-01 16:05:24 +0800

    agile

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myappApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', ['$scope','$http','$window','leafletData',function ($scope, $http,$window,leafletData) {
   	$scope.center = {
 	    lat: 22.280727,
 	    lng: 114.165079,
@@ -13,7 +13,56 @@ angular.module('myappApp')
             reuseTiles:true,
             maxZoom: 17,
             minZoom:15
-    }
+    };
+
+console.log(leafletData);
+    var promise =  leafletData.getMap();
+promise.finally(function() {
+    console.log('failed');
+})
+// $scope.doSth = function() {
+leafletData.getMap('map').then(function(map) {
+    console.log(map);
+$window.L.control.locate({
+    position: 'topleft',  // set the location of the control
+    drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
+    follow: false,  // follow the user's location
+    setView: true, // automatically sets the map view to the user's location, enabled if `follow` is true
+    locateOptions: {}  // define location options e.g enableHighAccuracy: true or maxZoom: 10
+}).addTo(map);
+});
+    
+// };
+
+
+// $window.L.control.locate({
+//     position: 'topleft',  // set the location of the control
+//     drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
+//     follow: false,  // follow the user's location
+//     setView: true, // automatically sets the map view to the user's location, enabled if `follow` is true
+//     keepCurrentZoomLevel: false, // keep the current map zoom level when displaying the user's location. (if `false`, use maxZoom)
+//     stopFollowingOnDrag: false, // stop following when the map is dragged if `follow` is true (deprecated, see below)
+//     // markerClass: L.circleMarker, // L.circleMarker or L.marker
+//     circleStyle: {},  // change the style of the circle around the user's location
+//     markerStyle: {},
+//     followCircleStyle: {},  // set difference for the style of the circle around the user's location while following
+//     followMarkerStyle: {},
+//     icon: 'icon-location',  // `icon-location` or `icon-direction`
+//     iconLoading: 'icon-spinner  animate-spin',  // class for loading icon
+//     circlePadding: [0, 0], // padding around accuracy circle, value is passed to setBounds
+//     metric: true,  // use metric or imperial units
+//     onLocationError: function(err) {alert(err.message)},  // define an error callback function
+//     // onLocationOutsideMapBounds:  function(context) { // called when outside map boundaries
+//     //         alert(context.options.strings.outsideMapBoundsMsg);
+//     // },
+//     strings: {
+//         title: "Show me where I am",  // title of the locate control
+//         popup: "You are within {distance} {unit} from this point",  // text to appear if user clicks on circle
+//         outsideMapBoundsMsg: "You seem located outside the boundaries of the map" // default message for onLocationOutsideMapBounds
+//     }
+//     locateOptions: {}  // define location options e.g enableHighAccuracy: true or maxZoom: 10
+// }]).addTo($window.map);
+
 
     $scope.markerTypes = [
         { "key":"water", "checked": true, "name": "水機"},
@@ -47,7 +96,6 @@ angular.module('myappApp')
             iconAnchor:   [20, 19] // point of the icon which will correspond to marker's location            
         }
     };
-    console.log(local_icons);
   	$http.get('https://spreadsheets.google.com/feeds/list/0AgDEdGmA1LoQdHdScTBZUkF5ejJSV3lra0FJVUhxMGc/1/public/values?alt=json')
        .then(function(res){
 
@@ -93,4 +141,4 @@ angular.module('myappApp')
                     $scope.markers.push($scope.alldata[i]);    
     	}
     }
-  });
+  }]);
